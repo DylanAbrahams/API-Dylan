140dd9f1b5c3d2d7f01842442f03d1bb75ddd29f
DylanAbrahams <143963818+DylanAbrahams@users.noreply.github.com>
2026-05-04 02:06:35 +0200

    Type filter grass

--- public/game/grass.js
+++ public/game/grass.js
@@ -1,17 +1,23 @@
 export const grass = [
-    { x: 390, y: 325, w: 150, h: 90 }, // Modder
-
-    { x: 575, y: 450, w: 160, h: 120 }, // Gras linksboven
-
-    { x: 865, y: 290, w: 85, h: 90 },
-    { x: 895, y: 350, w: 190, h: 65 }, // Boven, 3 hitboxes
-    { x: 930, y: 415, w: 100, h: 30 },
-
-    { x: 1250, y: 350, w: 160, h: 65 },
-    { x: 1315, y: 415, w: 120, h: 260 }, // Midden, 3 hitboxes
-    { x: 1185, y: 575, w: 130, h: 130 },
-
-    { x: 1580, y: 705, w: 150, h: 125 }, // Rechtsonder
-
-    { x: 520, y: 770, w: 220, h: 60 }, // Linksonder
+    // Modder 
+    { x: 390, y: 325, w: 150, h: 90, types: ["ground", "rock"] }, 
+ 
+    // Gras linksboven 
+    { x: 575, y: 450, w: 160, h: 120, types: ["psychic", "poison", "ghost"] }, 
+ 
+    // Boven (3 hitboxes)
+    { x: 865, y: 290, w: 85, h: 90, types: ["flying", "normal"] },
+    { x: 895, y: 350, w: 190, h: 65, types: ["flying", "normal"] }, 
+    { x: 930, y: 415, w: 100, h: 30, types: ["flying", "normal"] },
+ 
+    // Midden (3 hitboxes)
+    { x: 1250, y: 350, w: 160, h: 65, types: ["grass", "bug"] },
+    { x: 1315, y: 415, w: 120, h: 260, types: ["grass", "bug"] },
+    { x: 1185, y: 575, w: 130, h: 130, types: ["grass", "bug"] },
+ 
+    // Rechts 
+    { x: 1580, y: 705, w: 150, h: 125, types: ["water", "ice", "dragon"] },
+ 
+    // Linksonder (Electric / Fire / Rock)
+    { x: 520, y: 770, w: 220, h: 60, types: ["electric", "fire"] },
 ];
